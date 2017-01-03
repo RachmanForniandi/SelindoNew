a4f78daec65852b0971436b92899183d9577efc5
RachmanForniandi <rachmanforniandi@gmail.com>
2017-01-03 15:29:47 +0700

    fix setting

--- app/src/main/java/com/selindonew/MainFragment.java
+++ app/src/main/java/com/selindonew/MainFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 /**
@@ -19,11 +22,31 @@ public class MainFragment extends Fragment {
     }
 
 
+    EditText eT1;
+    Button btn1;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        eT1 = (EditText) view.findViewById(R.id.eT1);
+        btn1 = (Button) view.findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainFragment.this.getActivity(), "Submitted", Toast.LENGTH_SHORT).show();
+                if (eT1.getText().toString().equals("10.8.12.5")) {
+                    Toast.makeText(MainFragment.this.getActivity(), "Koneksi IP Berhasil", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainFragment.this.getActivity(), "Koneksi IP Gagal", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        return view;
     }
-
 }
+
+
+
+
